9a8765156180a184725cffdc2a8466bce243a94a
adriann postigo <adriannparanhos@gmail.com>
2025-08-26 10:07:39 -0300

    feat(login): create login component with Google OAuth and automatic redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: LoginPageComponent },
+    { path: '**', redirectTo: 'login' }
+];
